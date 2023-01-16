0094f4778547824a1fd82d2463a64aa973e35740
Paul Landaeta <paul.landeata@coxautoinc.com>
2023-01-16 09:48:54 -0400

    feat: add operations

--- Calculadora/src/screens/Calculadora.tsx
+++ Calculadora/src/screens/Calculadora.tsx
@@ -5,12 +5,13 @@ import {ButtonC} from '../components/ButtonC';
 export const Calculadora = () => {
   const [number, setNumber] = useState('0');
   const [previous, setPrevious] = useState('0');
+  const [currentOperator, setCurrentOperator] = useState('');
   const pressNumber = (digit: string) => {
     if (digit === '.' && number.includes('.')) {
       return;
-    } else if (number.startsWith('0') && number.includes('.')) { 
+    } else if (number.startsWith('0') && number.includes('.')) {
       setNumber(number + digit);
-    } else if (number.startsWith('0') && !number.includes('.')) { 
+    } else if (number.startsWith('0') && !number.includes('.')) {
       if (digit === '.') {
         setNumber(number + digit);
       } else {
@@ -21,20 +22,47 @@ export const Calculadora = () => {
     }
   };
 
-  const pressClean = (operator: string ) => {
+  const pressClean = (operator: string) => {
     console.log(operator);
     setNumber('0');
   };
+
+  const pressOperation = (operator: string) => {
+    setCurrentOperator(operator);
+    setPrevious(number);
+    setNumber('0');
+  };
+
+  const result = (operator: string) => {
+    console.log(currentOperator);
+    if (currentOperator === '') {
+      setPrevious(number);
+      setNumber('0');
+    } else {
+      if (currentOperator === '+') {
+        setNumber((Number(previous) + Number(number)).toString());
+        setPrevious('0');
+      }
+    }
+  };
+
+  const pressSign = (operator: string) => {
+    if (number.startsWith('-')) {
+      setNumber(number.replace('-', ''));
+    } else {
+      setNumber('-' + number);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text 
-      numberOfLines={1}
-      adjustsFontSizeToFit
-      style={styles.previous}> {previous} </Text>
-      <Text 
-      numberOfLines={1}
-      adjustsFontSizeToFit
-      style={styles.result}> {number} </Text>
+      {previous !== '0' && (
+        <Text numberOfLines={1} adjustsFontSizeToFit style={styles.previous}>
+          {previous}
+        </Text>
+      )}
+      <Text numberOfLines={1} adjustsFontSizeToFit style={styles.result}>
+        {number}
+      </Text>
       <View style={styles.row}>
         <ButtonC
           text={'AC'}
@@ -42,33 +70,38 @@ export const Calculadora = () => {
           bColor="#9B9B9B"
           action={pressClean}
         />
-        <ButtonC text={'+/-'} colorText="black" bColor="#9B9B9B" />
+        <ButtonC
+          text={'+/-'}
+          colorText="black"
+          bColor="#9B9B9B"
+          action={pressSign}
+        />
         <ButtonC text={'%'} colorText="black" bColor="#9B9B9B" />
-        <ButtonC text={'/'} bColor="#FF9427" />
+        <ButtonC text={'/'} bColor="#FF9427" action={pressOperation} />
       </View>
       <View style={styles.row}>
         <ButtonC text={'7'} bColor="#2D2D2D" action={pressNumber} />
-        <ButtonC text={'8'} bColor="#2D2D2D" action={pressNumber}/>
-        <ButtonC text={'9'} bColor="#2D2D2D" action={pressNumber}/>
-        <ButtonC text={'X'} bColor="#FF9427" />
+        <ButtonC text={'8'} bColor="#2D2D2D" action={pressNumber} />
+        <ButtonC text={'9'} bColor="#2D2D2D" action={pressNumber} />
+        <ButtonC text={'X'} bColor="#FF9427" action={pressOperation} />
       </View>
       <View style={styles.row}>
-        <ButtonC text={'4'} bColor="#2D2D2D" action={pressNumber}/>
-        <ButtonC text={'5'} bColor="#2D2D2D" action={pressNumber}/>
-        <ButtonC text={'6'} bColor="#2D2D2D" action={pressNumber}/>
-        <ButtonC text={'-'} bColor="#FF9427" />
+        <ButtonC text={'4'} bColor="#2D2D2D" action={pressNumber} />
+        <ButtonC text={'5'} bColor="#2D2D2D" action={pressNumber} />
+        <ButtonC text={'6'} bColor="#2D2D2D" action={pressNumber} />
+        <ButtonC text={'-'} bColor="#FF9427" action={pressOperation} />
       </View>
       <View style={styles.row}>
-        <ButtonC text={'1'} bColor="#2D2D2D" action={pressNumber}/>
-        <ButtonC text={'2'} bColor="#2D2D2D" action={pressNumber}/>
-        <ButtonC text={'3'} bColor="#2D2D2D" action={pressNumber}/>
-        <ButtonC text={'+'} bColor="#FF9427" />
+        <ButtonC text={'1'} bColor="#2D2D2D" action={pressNumber} />
+        <ButtonC text={'2'} bColor="#2D2D2D" action={pressNumber} />
+        <ButtonC text={'3'} bColor="#2D2D2D" action={pressNumber} />
+        <ButtonC text={'+'} bColor="#FF9427" action={pressOperation} />
       </View>
       <View style={styles.row}>
-        <ButtonC text={'0'} bColor="#2D2D2D" action={pressNumber}/>
+        <ButtonC text={'0'} bColor="#2D2D2D" action={pressNumber} />
         <ButtonC text={'0'} bColor="#2D2D2D" />
-        <ButtonC text={'.'} bColor="#2D2D2D" action={pressNumber}/>
-        <ButtonC text={'='} bColor="#FF9427" />
+        <ButtonC text={'.'} bColor="#2D2D2D" action={pressNumber} />
+        <ButtonC text={'='} bColor="#FF9427" action={result} />
       </View>
     </View>
   );
