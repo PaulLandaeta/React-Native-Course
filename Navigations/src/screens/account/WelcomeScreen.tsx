e720ed05de9a66a466bd06faa0d4f7f449241931
Paul Landaeta <paul.landeata@coxautoinc.com>
2023-02-01 09:26:16 -0400

    feat: add firebase auth

--- Navigations/src/screens/account/WelcomeScreen.tsx
+++ Navigations/src/screens/account/WelcomeScreen.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
+import {onAuthStateChanged} from 'firebase/auth';
+import {auth} from './../../services/firebase';
+
 export const WelcomeScreen = () => {
-  return (
+  const [hasLogged, setHasLogged] = useState(false);
+
+  useEffect(() => {
+    // TODO: getAuth from Firebase
+    onAuthStateChanged(auth, (user: any) => {
+      console.log(user);
+      setHasLogged(user ? true : false);
+    });
+  }, []);
+  return hasLogged ? (
+    <View>
+      <Text>Screen Welcome Logged</Text>
+    </View>
+  ) : (
     <View>
       <Text>Screen Welcome</Text>
     </View>
--- Navigations/src/services/firebase.ts
+++ Navigations/src/services/firebase.ts
@@ -1,15 +1,17 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase/firestore";
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2Qq3mzPV6avaxfxeJj26HMkZE0_hIWNA",
-  authDomain: "products-34e09.firebaseapp.com",
-  projectId: "products-34e09",
-  storageBucket: "products-34e09.appspot.com",
-  messagingSenderId: "607364901551",
-  appId: "1:607364901551:web:d5d1e3a5771bafc9e4425a",
-  measurementId: "G-PDJ68HYC6E"
+  apiKey: 'AIzaSyC2Qq3mzPV6avaxfxeJj26HMkZE0_hIWNA',
+  authDomain: 'products-34e09.firebaseapp.com',
+  projectId: 'products-34e09',
+  storageBucket: 'products-34e09.appspot.com',
+  messagingSenderId: '607364901551',
+  appId: '1:607364901551:web:d5d1e3a5771bafc9e4425a',
+  measurementId: 'G-PDJ68HYC6E',
 };
 
 export const initFirebase = initializeApp(firebaseConfig);
+export const auth = getAuth(initFirebase);
 export const db = getFirestore(initFirebase);
