65390d5e11ca9bca0e2366844a9ee38a216db757
Paul Landaeta <paul.landeata@coxautoinc.com>
2023-02-01 10:12:50 -0400

    feat: login user

--- Navigations/src/screens/account/LoginScreen.tsx
+++ Navigations/src/screens/account/LoginScreen.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import {Input, Button} from '@rneui/themed';
+import {signInWithEmailAndPassword} from 'firebase/auth';
+import {auth} from './../../services/firebase';
+
 export const LoginScreen = () => {
+  const login = () => {
+    signInWithEmailAndPassword(auth, 'paulwilkerlf@gmail.com', '123456')
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorMessage);
+      });
+  };
   return (
-    <View>
-      <Text>Screen Login</Text>
-    </View>
+    <>
+      <View style={{flex: 1, backgroundColor: 'red'}}>
+        <View style={{flex: 1, backgroundColor: 'black'}}></View>
+        <View style={{flex: 1, backgroundColor: 'white'}}>
+          <Input placeholder="Email" />
+          <Input placeholder="Password" secureTextEntry={true} />
+          <Button
+            title="LOG IN"
+            buttonStyle={{
+              backgroundColor: 'black',
+              borderWidth: 2,
+              borderColor: 'white',
+              borderRadius: 30,
+            }}
+            containerStyle={{
+              width: 200,
+              marginHorizontal: 50,
+              marginVertical: 10,
+            }}
+            titleStyle={{fontWeight: 'bold'}}
+            onPress={login}
+          />
+        </View>
+      </View>
+    </>
   );
 };
--- Navigations/src/screens/account/WelcomeScreen.tsx
+++ Navigations/src/screens/account/WelcomeScreen.tsx
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 import {onAuthStateChanged} from 'firebase/auth';
 import {auth} from './../../services/firebase';
+import {Button, ButtonGroup, withTheme} from '@rneui/themed';
 
-export const WelcomeScreen = () => {
+export const WelcomeScreen = props => {
+  const {navigation} = props;
   const [hasLogged, setHasLogged] = useState(false);
-
+  const goToLogin = () => {
+    navigation.navigate('LoginScreen');
+  };
   useEffect(() => {
-    // TODO: getAuth from Firebase
     onAuthStateChanged(auth, (user: any) => {
       console.log(user);
       setHasLogged(user ? true : false);
@@ -20,6 +23,23 @@ export const WelcomeScreen = () => {
   ) : (
     <View>
       <Text>Screen Welcome</Text>
+      <Button
+        title="Log in"
+        loading={false}
+        loadingProps={{size: 'small', color: 'white'}}
+        buttonStyle={{
+          backgroundColor: 'rgba(111, 202, 186, 1)',
+          borderRadius: 5,
+        }}
+        titleStyle={{fontWeight: 'bold', fontSize: 23}}
+        containerStyle={{
+          marginHorizontal: 50,
+          height: 50,
+          width: 200,
+          marginVertical: 10,
+        }}
+        onPress={goToLogin}
+      />
     </View>
   );
 };
